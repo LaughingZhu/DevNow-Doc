4f37ca8858c1aee31b29cbeed898d30d939fbe40
zhuxiaochen <474268433@qq.com>
2024-12-20 19:25:18 +0800

    feat: upload header

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,67 +7,70 @@ const ogImageAlt = 'DevNow Doc OG Image';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			logo: {
-				src: './public/logo.webp',
-				alt: 'DevNow Logo',
-			},
+    integrations: [starlight({
+        logo: {
+            src: './public/logo.webp',
+            alt: 'DevNow Logo',
+        },
 			title: 'DevNow Docs',
-			social: {
-				github: 'https://github.com/LaughingZhu/DevNow',
-			},
-			
-			head: [
-				{
-					tag: 'script',
-					attrs: {
-						src: 'https://cdn.usefathom.com/script.js',
-						'data-site': 'EZBHTSIG',
-						defer: true,
-					},
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image', content: ogUrl },
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image:alt', content: ogImageAlt },
-				},
-			],
+        social: {
+            github: 'https://github.com/LaughingZhu/DevNow',
+        },
+        components: {
+					// 重写默认的 `SocialIcons` 组件。
+					SocialIcons: './src/components/SocialIcons.astro',
+        },
+        
+        head: [
+            {
+                tag: 'script',
+                attrs: {
+                    src: 'https://cdn.usefathom.com/script.js',
+                    'data-site': 'EZBHTSIG',
+                    defer: true,
+                },
+            },
+            {
+                tag: 'meta',
+                attrs: { property: 'og:image', content: ogUrl },
+            },
+            {
+                tag: 'meta',
+                attrs: { property: 'og:image:alt', content: ogImageAlt },
+            },
+        ],
 
-			favicon: '/public/favicon.svg',
-			sidebar: [
-				{
-					label: '简介',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: '为什么', slug: 'guides/why' },
-						{ label: '有哪些特性', slug: 'guides/feature' },
-					],
-				},
-				{
-					label: '指南',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: '功能',
-					autogenerate: { directory: 'feature' },
-				},
-				{
-					label: '部署',
-					items: [
-						{ label: '必看', slug: 'deploy' },
-						{ label: 'Vercel 部署', slug: 'deploy/vercel' },
-						{ label: 'CloudFlare 部署', slug: 'deploy/cloudflare' },
-					],
-				},
-				{
-					label: '扩展',
-					autogenerate: { directory: 'extend', collapsed: true },
-				}
-			],
+        favicon: '/public/favicon.svg',
+        sidebar: [
+            {
+                label: '简介',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: '为什么', slug: 'guides/why' },
+                    { label: '有哪些特性', slug: 'guides/feature' },
+                ],
+            },
+            {
+                label: '指南',
+                autogenerate: { directory: 'reference' },
+            },
+            {
+                label: '功能',
+                autogenerate: { directory: 'feature' },
+            },
+            {
+                label: '部署',
+                items: [
+                    { label: '必看', slug: 'deploy' },
+                    { label: 'Vercel 部署', slug: 'deploy/vercel' },
+                    { label: 'CloudFlare 部署', slug: 'deploy/cloudflare' },
+                ],
+            },
+            {
+                label: '扩展',
+                autogenerate: { directory: 'extend', collapsed: true },
+            }
+        ],
 		}),
 	],
 });
